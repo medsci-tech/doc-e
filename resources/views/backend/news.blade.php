7584ffb2ddd9f97ba0c338b9ac3ce53511858b89
余鹏飞 <501254061@qq.com>
2016-05-12 15:22:16 +0800

    test

---
+++ resources/views/backend/news.blade.php
@@ -0,0 +1,17 @@
+@extends('/layouts/app')
+
+@section('title','新闻信息表')
+
+@section('content-header')
+  <h1>
+    新闻信息表
+  </h1>
+  <ol class="breadcrumb">
+    <li><a href="#"><i class="fa fa-dashboard"></i>主页</a></li>
+    <li class="active">新闻信息表</li>
+  </ol>
+@endsection
+
+@section('content')
+
+@endsection
