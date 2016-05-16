6b2a592d5a588a3b80f8f55eef6395e94bbf6420
余鹏飞 <501254061@qq.com>
2016-05-16 16:57:31 +0800

    adjust

--- resources/views/article/create.blade.php
+++ resources/views/article/create.blade.php
@@ -41,7 +41,7 @@
 @section('content')
   <div class="row">
     <div class="col-xs-12">
-      <div class="box">
+      <div class="box box-primary">
         <div class="box-header">
         </div><!-- /.box-header -->
         <div class="box-body">
