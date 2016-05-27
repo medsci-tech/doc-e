83df5eecd37e4dea5daaddaf80f4e5ca2f56b575
蔡乾 <cai.q@foxmail.com>
2016-05-27 15:21:55 +0800

    finished controller

--- app/Doce/Article/Article.php
+++ app/Doce/Article/Article.php
@@ -16,6 +16,11 @@ class Article extends Model
     use SoftDeletes;
 
     /**
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\HasMany
      */
     public function keywords()
@@ -32,10 +37,53 @@ class Article extends Model
     }
 
     /**
+     * @param $plain_data
+     * @param $keywords
+     * @param $tags
+     * @return Article|null|mixed
+     */
+    public static function createUsingCredentials($plain_data, $keywords, $tags)
+    {
+        return \DB::transaction(function () use ($plain_data, $keywords, $tags) {
+            $article = self::create($plain_data);
+
+            foreach ($keywords as $keyword_name) {
+                $keyword = new Keyword();
+                $keyword->name = $keyword_name;
+                $article->keywords()->save($keyword);
+            }
+
+            foreach ($tags as $tag_name) {
+                $tag = new Tag();
+                $tag->name = $tag_name;
+                $article->tags()->save($tag);
+            }
+
+            return $article;
+        });
+    }
+
+    /**
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
      */
     public function creator()
     {
         return $this->belongsTo(User::class, 'creator_id');
+    }
+
+
+    /**
+     * @param array $credentials
+     * @return Article|null|mixed
+     */
+    public static function createUsingFormData(array $credentials)
+    {
+        $plain_data = collect($credentials)->only(['title', 'abstract', 'thumbnail_url', 'content'])->toArray();
+        $keywords = $credentials['keywords'];
+        $tags = $credentials['tags'];
+
+        $article = self::createUsingCredentials($plain_data, $keywords, $tags);
+
+        return $article;
     }
 }
--- app/Http/Controllers/Article/ArticleController.php
+++ app/Http/Controllers/Article/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Article;
 
+use App\Doce\Article\Article;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,13 +10,20 @@ use App\Http\Controllers\Controller;
 
 class ArticleController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('article.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        return 'TBD';
+        return Article::createUsingFormData($request->only([
+            'title', 'abstract', 'thumbnail_url', 'category', 'keywords', 'tags', 'content'
+        ]));
     }
 }
